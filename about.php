34acbfd522c11faf7f34f068a257ed8fdb4e0417
Kevin Conley <kevincon@users.noreply.github.com>
2009-10-11 17:15:27 +0000

    adding about.php
    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@55 96535c52-2767-499a-a306-8e95c0417af2

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php #script 7.3 register.php
+
+$page_title = 'About';
+include('header.html');
+			echo'<div id ="container">
+				<div id="spiel">
+					<p>IvyStones is a scavenger hunt to find the many Ivy Day stones hidden around the University of Pennsylvania campus.</p>
+				</div>
+			</div>
+				';
+?>	
+		</div>
+	</body>
+</html>
