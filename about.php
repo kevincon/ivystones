f3f184826fbf4731343b679cff30bbf88763cd6e
Kevin Conley <kevincon@users.noreply.github.com>
2009-10-11 20:24:29 +0000

    adding 1940stone.jpg
    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@87 96535c52-2767-499a-a306-8e95c0417af2

--- about.php
+++ about.php
@@ -7,6 +7,7 @@ include('header.php');
 				<div align="left">
 					<h1>About IvyStones</h1>				
 					<p>An ivy stone or Ivy Day stone is a class memorial stone unveiled at annual Ivy Day celebrations at several American universities and colleges that are in the Ivy League.  They are often placed on the sides of academic buildings. Since 1873 at the University of Pennsylvania, students have presented class ivy stones at the annual ivy day before commencement.</p>
+					<img src="images/1940stone.jpg" border="5">
 					<br>
 					<p>IvyStones.com is a scavenger hunt to find the many Ivy Day stones hidden around the University of Pennsylvania campus.  Players can create an account and keep track of the stones that they've found as well as compare their finds with other players.</p>
 					<h1>About the Creators</h1>
