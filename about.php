c0b7d0504426fcebffecb765394579f08578eb25
Kevin Conley <kevincon@users.noreply.github.com>
2009-10-11 17:58:14 +0000

    editing about.php
    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@67 96535c52-2767-499a-a306-8e95c0417af2

--- about.php
+++ about.php
@@ -10,7 +10,8 @@ include('header.html');
 					<br>
 					<p>IvyStones.com is a scavenger hunt to find the many Ivy Day stones hidden around the University of Pennsylvania campus.  Players can create an account and keep track of the stones that they've found as well as compare their finds with other players.</p>
 					<h1>About the Creators</h1>
-					<p>IvyStones was created by Sam Appelbaum, Kevin Conley, and Varun Sampath for the University of Pennsylvania's first <a href="http://www.pennapps.com">PennApps competition</a> in 2009.</p>  
+					<p>IvyStones was created by Sam Appelbaum, Kevin Conley, and Varun Sampath for the University of Pennsylvania's first <a href="http://www.pennapps.com">PennApps competition</a> in 2009.</p>
+					<img src="images/team.jpg" border="5">
 				</div>
 			</div>	
 		</div>
