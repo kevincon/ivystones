553ae8b83560c3b23d531e606257aa75d69ba677
Varun Sampath <vsampath@users.noreply.github.com>
2009-10-12 00:27:08 +0000

    did some floating...
    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@89 96535c52-2767-499a-a306-8e95c0417af2

--- about.php
+++ about.php
@@ -5,14 +5,14 @@ include('header.php');
 ?>
 			<div id ="content">
 				<div align="left">
-					<h1>About IvyStones</h1>				
+					<h1>About IvyStones</h1>	
+					<img src="images/1940stone.jpg" border="5" style="float: right; margin-left: 5px;" />					
 					<p>An ivy stone or Ivy Day stone is a class memorial stone unveiled at annual Ivy Day celebrations at several American universities and colleges that are in the Ivy League.  They are often placed on the sides of academic buildings. Since 1873 at the University of Pennsylvania, students have presented class ivy stones at the annual ivy day before commencement.</p>
-					<img src="images/1940stone.jpg" border="5">
 					<br>
 					<p>IvyStones.com is a scavenger hunt to find the many Ivy Day stones hidden around the University of Pennsylvania campus.  Players can create an account and keep track of the stones that they've found as well as compare their finds with other players.</p>
 					<h1>About the Creators</h1>
+					<img src="images/team.jpg" border="5" style="float: left; margin: 0 15px 20px 0; " />
 					<p>IvyStones was created by Sam Appelbaum, Varun Sampath, and Kevin Conley for the University of Pennsylvania's first <a href="http://www.pennapps.com">PennApps competition</a> in 2009.</p>
-					<img src="images/team.jpg" border="5">
 				</div>
 			</div>	
 		</div>
