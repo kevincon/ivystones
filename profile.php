b14bf2204736bcb23e87e0a906aa2f8452f6798b
Varun Sampath <vsampath@users.noreply.github.com>
2009-10-15 06:53:30 +0000

    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@97 96535c52-2767-499a-a306-8e95c0417af2

--- profile.php
+++ profile.php
@@ -1,13 +1,10 @@
 <?php #script 7.3 register.php
 
-session_name('YourVisitID');
-session_start();
-
 $page_title = 'Profile';
 include('header.php');
 ?>
 			<div id ="content">
-					<p>Nothing here.</p>
+					<h1><?php echo $name."'s profile"; ?></h1>
 			</div>
 		</div>
 	</body>
