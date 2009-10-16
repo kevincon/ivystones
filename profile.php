577100c2f418c30406aca32b8dfae5168d9dd133
Varun Sampath <vsampath@users.noreply.github.com>
2009-10-16 19:49:26 +0000

    Profile done, but with no image support
    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@109 96535c52-2767-499a-a306-8e95c0417af2

--- profile.php
+++ profile.php
@@ -8,12 +8,30 @@ include('header.php');
 			<div id ="content">
 					<?php 
 						if (isset($name)) {
-							echo "<h1>$name's profile<h1>";
-				
+							echo "<h3 style=\"float: right;\"><a href=\"stones.php\" style=\"text-decoration: underline;\">Log or View Stones!</a></h3>";
+							echo "<h1>$name's profile</h1>";
+							
 							$pro_result = mysql_query("SELECT stone_id, date_found, location, notes FROM findings WHERE user_id = '$uid'");
+							$num = mysql_num_rows($pro_result);
+							
+							if ($num == 0)
+								echo "<h2>No finds yet, log some <a href=\"stones.php\" style=\"text-decoration: underline;\">here</a>!</h2>";
+							echo "<ul class=\"biglist\">";
 							while ($row = mysql_fetch_array($pro_result, MYSQL_ASSOC)) {
-								echo "<p>ID: $row[0]  Date: $row[1]</p>";  
+								$stone = $row['stone_id'];
+								$date = $row['date_found'];
+								$location = $row['location'];
+								$notes = $row['notes'];
+								
+								echo "<li class=\"find\">";
+								echo "<img class=\"find_img\" src=\"images/d_silhouette.gif\" alt=\"$stone Image\" />";
+								echo "<span class=\"find_stone\">Class of $stone, found $date at $location</span>";
+								echo "<p class=\"find_desc\">";
+								echo "<span style=\"font-style: normal;\">Find description: </span>".$notes."</p>";
+								echo "</li>";
 							}
+							echo "</ul>";
+
 						}
 						else
 							echo "<h1>Log in first!</h1>";
