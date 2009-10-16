e69116e4b6036e5c530073d106c2cd005644cdee
Varun Sampath <vsampath@users.noreply.github.com>
2009-10-16 19:06:12 +0000

    Profile in progress
    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@108 96535c52-2767-499a-a306-8e95c0417af2

--- profile.php
+++ profile.php
@@ -1,12 +1,20 @@
 <?php #script 7.3 register.php
 
+require_once('mysql_connect.php'); //connect to db
+
 $page_title = 'Profile';
 include('header.php');
 ?>
 			<div id ="content">
 					<?php 
-						if (isset($name))
+						if (isset($name)) {
 							echo "<h1>$name's profile<h1>";
+				
+							$pro_result = mysql_query("SELECT stone_id, date_found, location, notes FROM findings WHERE user_id = '$uid'");
+							while ($row = mysql_fetch_array($pro_result, MYSQL_ASSOC)) {
+								echo "<p>ID: $row[0]  Date: $row[1]</p>";  
+							}
+						}
 						else
 							echo "<h1>Log in first!</h1>";
 					?>
