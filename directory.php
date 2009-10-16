af11f3970bf767dadd1dc7730e360e74e22a03d3
Varun Sampath <vsampath@users.noreply.github.com>
2009-10-16 20:34:55 +0000

    git-svn-id: file:///mnt/x/Users/Kevin/Desktop/ivystones-svn@116 96535c52-2767-499a-a306-8e95c0417af2

--- directory.php
+++ directory.php
@@ -30,7 +30,7 @@ include('header.php');
 							if (file_exists($pic))
 								echo "<img class=\"find_img\" src=\"images/$pic\" alt=\"$stone Image\" />";
 							else
-								echo "<img class=\"find_img\" src=\"images/d_silhouette.gif\" alt=\"$stone Image\" />";
+								echo "<img class=\"find_img\" src=\"images/d_silhouette.gif\" alt=\"$stone Image Replacement\" />";
 							echo "<span class=\"find_stone\">Class of $stone, found $date at $location by $fn $ln</span>";
 							echo "<p class=\"find_desc\">";
 							echo "<span style=\"font-style: normal;\">Find description: </span>".$notes."</p>";
